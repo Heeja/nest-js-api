d160a56f08866e67a77e212114772939524461b8
Heeja <holeonline@naver.com>
2023-10-26 18:33:01 +0900

    #4 E2E Testing. get, post, delete request test in '/movies'.

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -19,6 +19,30 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Welcome Movie API(NestJS).');
+  });
+
+  describe('/movies', () => {
+    it('GET', () => {
+      return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
+      // 데이터가 없을 것이기 때문에 기대값을 빈배열([])을 입력했다.
+    });
+
+    it('POST', () => {
+      const movieInfo = {
+        name: '테스트용 영화정보',
+        year: 2023,
+        genres: ['Test'],
+      };
+
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send(movieInfo)
+        .expect(201);
+    });
+
+    it('DELETE', () => {
+      return request(app.getHttpServer()).delete('/movies').expect(404);
+    });
   });
 });
